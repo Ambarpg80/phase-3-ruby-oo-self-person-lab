9e6d8a834252a843e0c3733bf252fc1d996fb301
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-03-07 22:51:11 -0500

    commit tree 4a08c9b5bb910d3379fc8638db946cac7f77c997

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,81 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name 
+    # attr_writer 
+    attr_accessor :bank_account, :happiness, :hygiene, :happy
+   
+    def initialize(name)
+        
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+   end
+  
+   def happiness=(number)
+       @happiness = number.clamp(0,10) 
+    end
+
+    def hygiene=(number)
+       @hygiene = number.clamp(0,10) 
+    end
+
+    def happy?
+        happy = @happiness > 7 ? true : false
+    end          
+
+    def clean? 
+        clean = @hygiene > 7 ? true : false
+    end
+
+    def get_paid(salary)
+        @bank_account = @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+       if hygiene=(self.hygiene += 4)
+       return "♪ Rub-a-dub just relaxing in the tub ♫"
+       else 
+        return 10
+      end
+    end
+
+    def work_out
+        if hygiene=(self.hygiene -= 3) 
+            happiness=(self.happiness += 2)
+            return "♪ another one bites the dust ♫"
+            else 
+             return @hygiene = 0 
+              @happiness = 10
+           end
+         end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{ self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+         case topic
+        when 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+           return  "blah blah partisan blah lobbyist"
+        when  'weather'
+            self.happiness += 1
+            friend.happiness += 1
+           return  "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
+        end
+      end
+    
+     
+            
+
+
+
+end
